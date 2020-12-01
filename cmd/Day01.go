0dabcf296284686b7417b51ffed8f91fc51a0662
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-01 09:12:09 +0000

    D1

--- cmd/Day01.go
+++ cmd/Day01.go
@@ -4,31 +4,56 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-var flagtests = []struct {
-	in  string
-	out int
-}{
-	{"%a", 2},
-	{"%a", 3},
+func binarySearch(a []int, search int) int {
+	mid := len(a) / 2
+	switch {
+	case len(a) == 0:
+		return -1 // not found
+	case a[mid] > search:
+		return binarySearch(a[:mid], search)
+	case a[mid] < search:
+		result := binarySearch(a[mid+1:], search)
+		if result >= 0 { // if anything but the -1 "not found" result
+			return result + mid + 1
+		}
+		return -1
+	default: // a[mid] == search
+		return mid
+	}
 }
 
-func tests() bool {
-	passes := true
-	for _, tt := range flagtests {
-		res := solve(tt.in)
-		if res != tt.out {
-			fmt.Printf("Expected %v, got %v\n", tt.out, res)
-			passes = false
+func solve(input string) (int, int) {
+	part1 := -1
+	part2 := -1
+	ints := []int{}
+	for _, s := range strings.Split(input, "\n") {
+		i, _ := strconv.Atoi(s)
+		ints = append(ints, i)
+	}
+	ints = ints[:len(ints)-1]
+	sort.Ints(ints)
+	for idx, i := range ints {
+		res := binarySearch(ints, 2020-i)
+		if res != -1 && res != idx {
+			part1 = ints[res] * i
 		}
 	}
-	return passes
-}
 
-func solve(input string) int {
-	return 2
+	//Dumb solution, terrible complexity, don't care
+	for idx, i := range ints {
+		for idx2, i2 := range ints {
+			res := binarySearch(ints, 2020-i-i2)
+			if res != -1 && res != idx && res != idx2 {
+				part2 = ints[res] * i * i2
+			}
+		}
+	}
+	return part1, part2
 }
 
 func readInput(path string) string {
@@ -45,10 +70,7 @@ func readInput(path string) string {
 }
 
 func main() {
-	if !tests() {
-		fmt.Println("Tests are not passing!")
-		os.Exit(1)
-	}
-	input := readInput("./input.txt")
-	fmt.Printf("Solve: %v", solve(input))
+	input := readInput(fmt.Sprintf("../inputs/1.txt"))
+	p1, p2 := solve(input)
+	fmt.Printf("Part 1: %d, Part2: %d\n", p1, p2)
 }
