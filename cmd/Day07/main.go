11ab8f0dd1a82819b3dca8d8c0ed71933d526f10
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-07 20:38:27 +0000

    Extremely simple D07 visualization - part2 only

--- cmd/Day07/main.go
+++ cmd/Day07/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/bznein/AoC2020/pkg/input"
+	"github.com/bznein/AoC2020/pkg/term"
 )
 
 type bag struct {
@@ -20,6 +22,8 @@ type containRule struct {
 	b bag
 }
 
+var line int
+
 func target() bag {
 	return bag{
 		attribute: "shiny",
@@ -33,24 +37,42 @@ func (r rule) leadsToTarget(from bag) bool {
 		return false
 	}
 	for _, b := range v {
-		if b.b == target() {
-			return true
-		}
-		if r.leadsToTarget(b.b) {
+		if b.b == target() || r.leadsToTarget(b.b) {
 			return true
 		}
 	}
 	return false
 }
 
-func (r rule) exploreToEnd(from bag) int {
+func printBagAtLevel(b bag, depth int, n int) {
+	var sb strings.Builder
+	if depth > 0 {
+		for i := 0; i < 2*depth; i++ {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(" |")
+		for i := 0; i < 1; i++ {
+			sb.WriteString("-")
+		}
+		sb.WriteString("->")
+	}
+	sb.WriteString(fmt.Sprintf(" %d %s %s", n, b.attribute, b.colour))
+	term.Tbprint(0, line, term.White, term.Black, sb.String())
+	line += 1
+}
+
+func (r rule) exploreToEnd(from bag, nBag int, depth int) int {
 	n := 1
+	if input.Visualize {
+		printBagAtLevel(from, depth, nBag)
+		time.Sleep(time.Millisecond * input.Delay)
+	}
 	v, ok := r[from]
 	if !ok {
 		return n
 	}
 	for _, b := range v {
-		n += b.n * r.exploreToEnd(b.b)
+		n += b.n * r.exploreToEnd(b.b, b.n, depth+1)
 	}
 	return n
 }
@@ -58,9 +80,15 @@ func (r rule) exploreToEnd(from bag) int {
 func solve(inputF string) (int, int) {
 	part1 := 0
 	part2 := 0
+	line = 0
 
 	bags := input.InputToStringSlice(inputF)
+
 	contains := rule{}
+	if input.Visualize {
+		term.Init()
+		defer term.Close()
+	}
 
 	for _, b := range bags {
 		splitted := strings.Split(b, " ")
@@ -90,7 +118,7 @@ func solve(inputF string) (int, int) {
 			part1++
 		}
 		if k == target() {
-			part2 = contains.exploreToEnd(k) - 1
+			part2 = contains.exploreToEnd(k, 1, 0) - 1
 		}
 	}
 
