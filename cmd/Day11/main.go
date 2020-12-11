f8bdcf12bab1bd83c079993bbc8de6e2a0a4d457
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-11 17:29:28 +0000

    Day11

---
+++ cmd/Day11/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/bznein/AoC2020/pkg/algorithm"
+	"github.com/bznein/AoC2020/pkg/input"
+	"github.com/bznein/AoC2020/pkg/timing"
+)
+
+const (
+	floor    = '.'
+	empty    = 'L'
+	occupied = '#'
+)
+
+func occupiedAdjacentSeats(seats []string, row int, column int, maxLook int) int {
+	tot := 0
+
+	// Look up
+	for i := row - 1; i >= 0 && row-i <= maxLook; i-- {
+		if seats[i][column] == occupied {
+			tot++
+			break
+		} else if seats[i][column] == empty {
+			break
+		}
+	}
+	// Look down
+	for i := row + 1; i < len(seats) && i-row <= maxLook; i++ {
+		if seats[i][column] == occupied {
+			tot++
+			break
+		} else if seats[i][column] == empty {
+			break
+		}
+	}
+	// Look left
+	for j := column - 1; j >= 0 && column-j <= maxLook; j-- {
+		if seats[row][j] == occupied {
+			tot++
+			break
+		} else if seats[row][j] == empty {
+			break
+		}
+	}
+	// Look right
+	for j := column + 1; j < len(seats[row]) && j-column <= maxLook; j++ {
+		if seats[row][j] == occupied {
+			tot++
+			break
+		} else if seats[row][j] == empty {
+			break
+		}
+	}
+
+	// Look Upper Left
+	for i, j := row-1, column-1; i >= 0 && j >= 0 && row-i <= maxLook; i, j = i-1, j-1 {
+		if seats[i][j] == occupied {
+			tot++
+			break
+		} else if seats[i][j] == empty {
+			break
+		}
+	}
+
+	// Look Upper Right
+	for i, j := row-1, column+1; i >= 0 && j < len(seats[i]) && row-i <= maxLook; i, j = i-1, j+1 {
+		if seats[i][j] == occupied {
+			tot++
+			break
+		} else if seats[i][j] == empty {
+			break
+		}
+	}
+
+	// Look Lower Left
+	for i, j := row+1, column-1; i < len(seats) && j >= 0 && i-row <= maxLook; i, j = i+1, j-1 {
+		if seats[i][j] == occupied {
+			tot++
+			break
+		} else if seats[i][j] == empty {
+			break
+		}
+	}
+
+	// Look Lower Right
+	for i, j := row+1, column+1; i < len(seats) && j < len(seats[row]) && i-row <= maxLook; i, j = i+1, j+1 {
+		if seats[i][j] == occupied {
+			tot++
+			break
+		} else if seats[i][j] == empty {
+			break
+		}
+	}
+	return tot
+}
+
+func totalOccupied(seats []string) int {
+	tot := 0
+	for _, r := range seats {
+		for _, c := range r {
+			if c == occupied {
+				tot++
+			}
+		}
+	}
+	return tot
+}
+
+func solvePart1(seats []string) int {
+	defer timing.TimeTrack(time.Now())
+	for {
+		seatsTemp := make([]string, len(seats))
+		changes := false
+		for i, row := range seats {
+			var sb strings.Builder
+			for j, column := range row {
+				switch column {
+				case floor:
+					sb.WriteRune(floor)
+				case empty:
+					if occupiedAdjacentSeats(seats, i, j, 1) == 0 {
+						sb.WriteRune(occupied)
+						changes = true
+					} else {
+						sb.WriteRune(empty)
+					}
+				case occupied:
+					if occupiedAdjacentSeats(seats, i, j, 1) >= 4 {
+						sb.WriteRune(empty)
+						changes = true
+					} else {
+						sb.WriteRune(occupied)
+					}
+				}
+			}
+			seatsTemp[i] = sb.String()
+		}
+		if !changes {
+			return totalOccupied(seatsTemp)
+
+		}
+		copy(seats, seatsTemp)
+	}
+}
+
+func solvePart2(seats []string) int {
+	defer timing.TimeTrack(time.Now())
+	maxLook := algorithm.Max(len(seats), len(seats[0]))
+	for {
+		seatsTemp := make([]string, len(seats))
+		changes := false
+		for i, row := range seats {
+			var sb strings.Builder
+			for j, column := range row {
+				switch column {
+				case floor:
+					sb.WriteRune(floor)
+				case empty:
+					if occupiedAdjacentSeats(seats, i, j, maxLook) == 0 {
+						sb.WriteRune(occupied)
+						changes = true
+					} else {
+						sb.WriteRune(empty)
+					}
+				case occupied:
+					if occupiedAdjacentSeats(seats, i, j, maxLook) >= 5 {
+						sb.WriteRune(empty)
+						changes = true
+					} else {
+						sb.WriteRune(occupied)
+					}
+				}
+			}
+			seatsTemp[i] = sb.String()
+		}
+		if !changes {
+			return totalOccupied(seatsTemp)
+
+		}
+
+		copy(seats, seatsTemp)
+	}
+}
+
+func solve(inputF string) (int, int) {
+	part1 := 0
+	part2 := 0
+
+	n := input.InputToStringSlice(inputF)
+	part1 = solvePart1(n)
+	n = input.InputToStringSlice(inputF)
+	part2 = solvePart2(n)
+	return part1, part2
+}
+
+func main() {
+	input.ParseFlags()
+	inputF := input.ReadInput(fmt.Sprintf("../../inputs/11.txt"))
+
+	p1, p2 := solve(inputF)
+	fmt.Printf("Part 1: %d, Part2: %d\n", p1, p2)
+
+}
