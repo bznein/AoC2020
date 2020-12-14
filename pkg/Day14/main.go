e51938c1521a9220a6932544a43afc83b6e03328
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-14 16:58:31 +0000

    Refactoring and unit test

--- pkg/Day14/main.go
+++ pkg/Day14/main.go
@@ -31,31 +31,6 @@ func getMask2(s string) map[int]rune {
 	return m
 }
 
-func p1(n []string) int {
-
-	values := map[int]int64{}
-	var m map[int]rune
-	for i := 0; i < len(n); i++ {
-		if n[i][1] == 'a' {
-			//stupid, fast way to see when it is a mask
-			m = getMask1(n[i])
-			continue
-		}
-		cell := 0
-		val := 0
-		fmt.Sscanf(n[i], "mem[%d] = %d", &cell, &val)
-		valBinary := fmt.Sprintf("%036s", strconv.FormatInt(int64(val), 2))
-		valBinaryReplaced := s.ReplaceAtMap(valBinary, m)
-		values[cell], _ = strconv.ParseInt(valBinaryReplaced, 2, 64)
-	}
-
-	part1 := 0
-	for _, v := range values {
-		part1 += int(v)
-	}
-	return part1
-}
-
 func getAllPossibleStrings(ss string) []string {
 	found := false
 	retVal := []string{}
@@ -73,40 +48,42 @@ func getAllPossibleStrings(ss string) []string {
 	return retVal
 }
 
-func p2(n []string) int {
+func Solve(inputF string) (int, int) {
+	defer timing.TimeTrack(time.Now())
+	n := input.InputToStringSlice(inputF)
 
-	values := map[int]int64{}
-	var m map[int]rune
+	values1 := map[int]int64{}
+	var m1 map[int]rune
+	values2 := map[int]int64{}
+	var m2 map[int]rune
 	for i := 0; i < len(n); i++ {
 		if n[i][1] == 'a' {
 			//stupid, fast way to see when it is a mask
-			m = getMask2(n[i])
+			m1 = getMask1(n[i])
+			m2 = getMask2(n[i])
 			continue
 		}
 		cell := 0
 		val := 0
 		fmt.Sscanf(n[i], "mem[%d] = %d", &cell, &val)
+		valBinary := fmt.Sprintf("%036s", strconv.FormatInt(int64(val), 2))
+		valBinaryReplaced := s.ReplaceAtMap(valBinary, m1)
+		values1[cell], _ = strconv.ParseInt(valBinaryReplaced, 2, 64)
 		cellBinary := fmt.Sprintf("%036s", strconv.FormatInt(int64(cell), 2))
-		cellBinaryReplaced := s.ReplaceAtMap(cellBinary, m)
+		cellBinaryReplaced := s.ReplaceAtMap(cellBinary, m2)
 		for _, r := range getAllPossibleStrings(cellBinaryReplaced) {
 			address, _ := strconv.ParseInt(r, 2, 64)
-			values[int(address)] = int64(val)
+			values2[int(address)] = int64(val)
 		}
 	}
 
+	part1 := 0
+	for _, v := range values1 {
+		part1 += int(v)
+	}
 	part2 := 0
-	for _, v := range values {
+	for _, v := range values2 {
 		part2 += int(v)
 	}
-	return part2
-}
-
-func Solve(inputF string) (int, int) {
-	defer timing.TimeTrack(time.Now())
-	part1, part2 := 0, -1
-	n := input.InputToStringSlice(inputF)
-	part1 = p1(n)
-	part2 = p2(n)
-
 	return part1, part2
 }
