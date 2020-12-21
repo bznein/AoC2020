cdb1cbde73e27e4f3a756d8ce5a3c1e1e6afe361
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-21 09:14:24 +0000

    Day21 - to clean

--- pkg/Day21/main.go
+++ pkg/Day21/main.go
@@ -1,17 +1,116 @@
 package Day21
 
 import (
+	"fmt"
+	"sort"
+	"strings"
 	"time"
 
-	//"github.com/bznein/AoC2020/pkg/input"
+	"github.com/bznein/AoC2020/pkg/input"
 	"github.com/bznein/AoC2020/pkg/timing"
 )
 
+func contains(ss []string, s string) int {
+	for i, sss := range ss {
+		if s == sss {
+			return i
+		}
+	}
+	return -1
+}
+
+func remove(s []string, i int) []string {
+	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func fillAllergenes(maybe map[string][]string, temp map[string]string) map[string]string {
+	if len(maybe) == 0 {
+		return temp
+	}
+	for k, v := range maybe {
+		if len(v) == 1 {
+			temp[v[0]] = k
+			delete(maybe, k)
+			for k2, v2 := range maybe {
+				if idx := contains(v2, v[0]); idx != -1 {
+					maybe[k2] = remove(v2, idx)
+				}
+			}
+			return fillAllergenes(maybe, temp)
+		}
+	}
+	return temp
+}
+
 func Solve(inputF string) (int, int) {
 	defer timing.TimeTrack(time.Now())
-	part1, part2 := -1, -1
-	// n := input.InputToIntSlice(inputF)
-	// s := input.InputToStringSlice(inputF)
+	part1, part2 := 0, -1
+
+	allIngredients := map[string]bool{}
+	mayBeContainedIn := map[string][]string{}
+	s := input.InputToStringSlice(inputF)
+	for _, ss := range s {
+		ingSplit := strings.Split(ss, " (")
+		ingredients := strings.Split(ingSplit[0], " ")
+		for _, i := range ingredients {
+			allIngredients[i] = true
+		}
+		l := len(ingSplit[1])
+		allergenes := strings.Split(ingSplit[1][9:l-1], ", ")
+		for _, all := range allergenes {
+			if _, ok := mayBeContainedIn[all]; !ok {
+				for _, ing := range ingredients {
+					mayBeContainedIn[all] = append(mayBeContainedIn[all], ing)
+				}
+			} else {
+				newSlice := []string{}
+				for _, ing := range mayBeContainedIn[all] {
+					if idx := contains(ingredients, ing); idx == -1 {
+					} else {
+						newSlice = append(newSlice, ing)
+					}
+				}
+				mayBeContainedIn[all] = newSlice
+			}
+		}
+	}
+
+	notInAny := []string{}
+Search:
+	for v, _ := range allIngredients {
+
+		for _, all := range mayBeContainedIn {
+			for _, ingredient := range all {
+				if ingredient == v {
+					continue Search
+				}
+			}
+		}
+		notInAny = append(notInAny, v)
+	}
+
+	for _, ss := range s {
+		ingSplit := strings.Split(ss, " (")
+		ingredients := strings.Split(ingSplit[0], " ")
+		for _, ing := range ingredients {
+			if contains(notInAny, ing) != -1 {
+				part1++
+			}
+		}
+	}
+
+	finalAllergenes := fillAllergenes(mayBeContainedIn, map[string]string{})
+
+	finalIng := []string{}
+	for v, _ := range finalAllergenes {
+		finalIng = append(finalIng, v)
+	}
+	sort.Slice(finalIng, func(i, j int) bool {
+		return finalAllergenes[finalIng[i]] < finalAllergenes[finalIng[j]]
+	})
+
+	fmt.Printf("TODO remove me and change interface Final: %v\n", strings.Join(finalIng, ","))
 
 	return part1, part2
 }
