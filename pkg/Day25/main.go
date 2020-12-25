ce671f3409a8b3d07f5dda7945ab4d1c359a0781
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-25 10:50:52 +0000

    And it's a wrap :)

--- pkg/Day25/main.go
+++ pkg/Day25/main.go
@@ -3,15 +3,34 @@ package Day25
 import (
 	"time"
 
-	//	"github.com/bznein/AoC2020/pkg/input"
+	"github.com/bznein/AoC2020/pkg/input"
 	"github.com/bznein/AoC2020/pkg/timing"
 )
 
+func transform(loopSize int, subject int) int {
+	v := 1
+	for i := 0; i < loopSize; i++ {
+		v *= subject
+		v = v % 20201227
+	}
+	return v
+}
+
 func Solve(inputF string) (int, int) {
 	defer timing.TimeTrack(time.Now())
-	part1, part2 := -1, -1
-	// n := input.InputToIntSlice(inputF)
-	// s := input.InputToStringSlice(inputF)
+	n := input.InputToIntSlice(inputF)
 
-	return part1, part2
+	loopSize := 0
+	v := 1
+	for {
+		if v == n[0] {
+			return transform(loopSize, n[1]), -1
+		}
+		if v == n[1] {
+			return transform(loopSize, n[0]), -1
+		}
+		v *= 7
+		v = v % 20201227
+		loopSize++
+	}
 }
