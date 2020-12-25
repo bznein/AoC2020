cebc53bd3d345a8f5a545e1011c326344c903d85
Nikolas De Giorgis <nikolas.de-giorgis@10gen.com>
2020-12-25 10:56:59 +0000

    refactor :)

--- pkg/Day25/main.go
+++ pkg/Day25/main.go
@@ -7,30 +7,30 @@ import (
 	"github.com/bznein/AoC2020/pkg/timing"
 )
 
-func transform(loopSize int, subject int) int {
-	v := 1
-	for i := 0; i < loopSize; i++ {
-		v *= subject
-		v = v % 20201227
-	}
-	return v
-}
-
 func Solve(inputF string) (int, int) {
 	defer timing.TimeTrack(time.Now())
 	n := input.InputToIntSlice(inputF)
 
 	loopSize := 0
 	v := 1
+	n1Transformed := 1
+	n0Transformed := 1
 	for {
 		if v == n[0] {
-			return transform(loopSize, n[1]), -1
+			return n1Transformed, -1
 		}
 		if v == n[1] {
-			return transform(loopSize, n[0]), -1
+			return n0Transformed, -1
 		}
 		v *= 7
 		v = v % 20201227
+
+		n1Transformed *= n[1]
+		n1Transformed = n1Transformed % 20201227
+
+		n0Transformed *= n[0]
+		n0Transformed = n0Transformed % 20201227
+
 		loopSize++
 	}
 }
